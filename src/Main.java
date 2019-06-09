9262ce70138e719e2eec5c09b6571d5ce4dbadb6
DeMusi <demusi@yandex.ru>
2019-06-09 13:39:12 +0500

    Add Inner class DrawPanel

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,111 @@
-import Human.Human;
-
-import java.util.Scanner;
 import javax.sound.midi.*;
 import javax.swing.*;
+import java.awt.*;
+import java.io.*;
 
+public class Main implements ControllerEventListener
+{
+    static JFrame f = new JFrame("Мой первый мюзик клип сгенеренный кодом");
+    static MyDrawPanel ml;
 
-public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Main mini = new Main();
-       /* if(args.length < 2){
-            System.out.println("Не забудьте аргументы для инструмента и ноты");
-        }*/
-      //  else{
-            int instrument = 40;//Integer.parseInt(args[0]);
-            int note = 50;//Integer.parseInt(args[1]);
-            mini.play(instrument, note);
-        //}
-
-        /*System.out.println("Введи число!");
-        Scanner scn = new Scanner(System.in);
-
-        int b = scn.nextInt();
-        System.out.println(b);
-
-        Human H1 = new Human();
-
-        System.out.println("Введи имя человека!");
-        String name = scn.next();
-
-        System.out.println("Введи возраст и вес!");
-        int age= scn.nextInt(), weight = scn.nextInt();
-
-        H1.SetPerson(age, name, weight);*/
+        mini.go();
     }
 
-    public void play(int instrument, int note) {
+    public void setUpGui()
+    {
+        ml = new MyDrawPanel();
+        f.setContentPane(ml);
+        f.setBounds(30,30,300,300);
+        f.setVisible(true);
+    }
+
+    public void go()
+    {
+        setUpGui();
+        try{
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            player.addControllerEventListener(ml, new int[] {127});
+            Sequence seq = new Sequence(Sequence.PPQ,4);
+            Track track = seq.createTrack();
+
+            int r = 0;
+            for(int i = 0; i < 60; i+=4)
+            {
+                r = (int)((Math.random() * 50) + 1);
+                track.add(makeEvent(144, 1, r, 100, i));
+                track.add(makeEvent(176, 1, 127, 0, i));
+                track.add(makeEvent(128, 1, r, 100, i+2));
+            }
+
+            player.setSequence(seq);
+            player.start();
+            player.setTempoInBPM(220);
+        } catch(Exception ex) { ex.printStackTrace(); }
+        /*JFrame frame = new JFrame();
+        button = new JButton("click me");
+        button.addActionListener(this);
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);*/
+    }
+
+    public void controlChange(ShortMessage event)
+    {
+        System.out.println("Ля!");
+    }
+
+    public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick)
+    {
+        MidiEvent event = null;
+        try{
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one , two);
+            event = new MidiEvent(a, tick);
+        } catch(Exception e) {}
+        return event;
+    }
+/*
+    public void actionPerformed(ActionEvent event)
+    {
+        button.setText("Clicked lol!");
+    }*/
+
+    class MyDrawPanel extends JPanel implements ControllerEventListener
+    {
+        boolean msg = false;
+
+        public void controlChange(ShortMessage event)
+        {
+            msg = true;
+            repaint();
+        }
+
+        public void paintComponent(Graphics g)
+        {
+            if(msg)
+            {
+                Graphics2D g2 = (Graphics2D) g;
+                int r = (int) (Math.random() * 250);
+                int gr = (int) (Math.random() * 250);
+                int b = (int) (Math.random() * 250);
+
+                g.setColor(new Color(r, gr, b));
+
+                int ht = (int)((Math.random() * 120) + 10 );
+                int width = (int)((Math.random() * 120) + 10 );
+                int x = (int)((Math.random() * 40) + 10 );
+                int y = (int)((Math.random() * 40) + 10 );
+                g.fillRect(x, y, width, ht);
+                msg = false;
+            }
+        }
+    }
+   /* public void play(int instrument, int note) {
         try {
             Sequencer player = MidiSystem.getSequencer();
             player.open();
@@ -65,5 +135,5 @@ public class Main {
         } catch(Exception ex) {
             ex.printStackTrace();
         }
-    }
+    }*/
 }
