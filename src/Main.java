bb528802eff47a2c1d830ad34cd138b85c438300
DeMusi <demusi@yandex.ru>
2019-06-10 13:06:35 +0500

    Ability to select save file by DialogMenu

--- src/Main.java
+++ src/Main.java
@@ -235,25 +235,29 @@ public class Main
     public class MyReadListener implements ActionListener{
         public void actionPerformed(ActionEvent a){
             boolean[] checkboxState = null;
-            try {
-              FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser")) ;
-              ObjectInputStream is = new ObjectInputStream(fileIn);
-              checkboxState = (boolean[])is.readObject();
-            } catch(Exception ex) { ex.printStackTrace(); }
+            JFileChooser fileOpen = new JFileChooser();
+            fileOpen.showOpenDialog(theFrame);
+            if(fileOpen.getSelectedFile() != null) {
+                try {
+                    FileInputStream fileIn = new FileInputStream(fileOpen.getSelectedFile());
+                    ObjectInputStream is = new ObjectInputStream(fileIn);
+                    checkboxState = (boolean[]) is.readObject();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
 
-            // Восстанавливаем состояние каждого чекбокса
-            for(int i = 0; i < 256; i++)
-            {
-                JCheckBox check = (JCheckBox)checkboxList.get(i);
-                if(checkboxState[i]){
-                    check.setSelected(true);
+                // Восстанавливаем состояние каждого чекбокса
+                for (int i = 0; i < 256; i++) {
+                    JCheckBox check = (JCheckBox) checkboxList.get(i);
+                    if (checkboxState[i]) {
+                        check.setSelected(true);
+                    } else {
+                        check.setSelected(false);
+                    }
                 }
-                else{
-                    check.setSelected(false);
-                }
+                sequencer.stop();
+                buildTrackAndStart();
             }
-            sequencer.stop();
-            buildTrackAndStart();
         }
     }
 }
